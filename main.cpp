1d7433c5834f6b45c5afd0e529b584c11a03cd1e
PainIam <tensie334@gmail.com>
2025-09-20 21:00:04 +0200

    ADD movement

--- main.cpp
+++ main.cpp
@@ -7,6 +7,25 @@ int main() {
     sf::VideoMode vm(WIDTH, HEIGHT);
     sf::RenderWindow window(vm, "Pong", sf::Style::Default);
 
+    Bat bat(WIDTH / 2 - 100, HEIGHT-90);
+    sf::Clock clock;
+    while(window.isOpen()) {
+        
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        sf::Time dt = clock.restart();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            bat.moveRight();
+            bat.update(dt);
+        }
 
 
+        window.clear();
+        window.draw(bat.getShape());
+        window.display();
+    }
 }
